20bb8b324b4a2dcf059a6a1c2de3da5ceb072780
Your Name <you@example.com>
2017-04-20 17:25:19 +0200

    A la semaine prochaine

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 #include "mnist.h"
 #include <limits.h>
 
-float linear_classifier(float *w, float* x){
-	float d=0;
+float linear_classifier(float* w, float* x){
+	    float d=0;
 		for (int i=0;i<784;i++){
 			 d += w[i]*x[i];
 		}
 		if (d>=0) return 1;
-		else return 0;
+		else return -1;
 }
 
 
@@ -22,8 +22,7 @@ float linear_classifier(float *w, float* x){
 }
 */
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
 
     float** images = read_mnist("train-images.idx3-ubyte");
 	float* labels = read_labels("train-labels.idx1-ubyte");
@@ -32,15 +31,33 @@ int main(int argc, char** argv)
     	
 		
 		float *w = new float[784];
-		for (int i=0; i>784; i++) w[i]=(float)rand()*2/INT_MAX - 1;
-			float E=0;
+
+		//step 1 : intialization
+		for (int i=0; i<784; i++) w[i]=(float)rand()*2/INT_MAX - 1;
+		float gamma = 0.01;	
+		//step 2 : Learning ( train)
+		for (int i=0; i<60000; i++) {
+			//Calcul gradiant (g = y*x si erreur sinon 0)
+			int prediction = linear_classifier(w,images[i]);
+			int verite = (labels[i] == 1) ? 1 : -1;
+				if (verite!=prediction) {
+				printf("ERREUR\n");
+			//j'ajoute au w actuel (w(t+1) = w(t) - gamma*y*x)
+				for (int j=0; j<784; j++)
+					w[j] = w[j] + gamma*verite*images[i][j];
+		}
+}
+		//step 3 : Test (t10k)
+		float E=0;
 			for(int i=0; i<10000; i++) {
 				printf("%u\n",i);
-				float inference = linear_classifier(w,test_images[i]);
+				int inference = linear_classifier(w,test_images[i]);
 				save_jpg(test_images[i], 28, 28, "%u/%u.jpg", inference, i);
-			}
-		}
-
+			
+				if((inference==1 && test_labels[i]!=1) || (inference==0 && test_labels[i]==1)) E++;
+				printf("Erreur = %0.2f%%\n", (E*100)/i);
+			 }
+		
 
 
 /*int NN;
@@ -61,9 +78,7 @@ int main(int argc, char** argv)
 
 */
 	
-
-			
-	 return 0;
+return 0;
 }
 
 
